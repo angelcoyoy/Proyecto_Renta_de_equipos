a13a827202fee1d361d94947ed97e16088583668
angel <angelcoyoy713@outlook.com>
2018-11-06 21:40:31 -0600

    Implementacion de Bootstrap, diseño de formularios de listar, agregar y editar.

--- Renta/forms.py
+++ Renta/forms.py
@@ -5,22 +5,56 @@ class ClienteForm(forms.ModelForm):
 
     class Meta:
         model = Cliente
-        fields = ('nombre', 'nit', 'telefono', 'direccion', 'correo', 'fecha_de_rentacion', 'equipos')
-
-        def __init__ (self, *args, **kwargs):
-            super(ClienteForm, self).__init__(*args, **kwargs)
-            self.fields["equipos"].widget = forms.widgets.CheckboxSelectMultiple()
-            self.fields["equipos"].help_text = "Ingrese los Equipos a rentar"
-            self.fields["equipos"].queryset = Equipo.objects.all()
+        fields = [
+            'nombre',
+            'nit',
+            'telefono',
+            'direccion',
+            'correo',
+            'fecha_de_rentacion',
+            'equipos',
+        ]
+        labels = {
+            'nombre': 'Nombre',
+            'nit': 'NIT',
+            'telefono': 'Telefono',
+            'direccion': 'Direccion',
+            'correo': 'Correo',
+            'fecha_de_rentacion': 'Fecha de rentacion de equipos',
+            'equipos': 'Equipos rentados',
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class':'form-control'}),
+            'nit': forms.TextInput(attrs={'class':'form-control'}),
+            'telefono': forms.TextInput(attrs={'class':'form-control'}),
+            'direccion': forms.TextInput(attrs={'class':'form-control'}),
+            'correo': forms.TextInput(attrs={'class':'form-control'}),
+            'fecha_de_rentacion': forms.TextInput(attrs={'class':'form-control'}),
+            'equipos' : forms.CheckboxSelectMultiple(),
+        }
 
 class EquipoForm(forms.ModelForm):
 
     class Meta:
         model = Equipo
-        fields = ('nombre', 'tipo', 'marca', 'precio', 'modelo')
-
-        def __init__ (self, *args, **kwargs):
-            super(EquipoForm, self).__init__(*args, **kwargs)
-            self.fields["clientes"].widget = forms.widgets.CheckboxSelectMultiple()
-            self.fields["clientes"].help_text = "Ingrese los clientes de los equipos rentados"
-            self.fields["clientes"].queryset = Cliente.objects.all()
+        fields = [
+            'nombre',
+            'tipo',
+            'marca',
+            'modelo',
+            'precio',
+        ]
+        labels = {
+            'nombre': 'Nombre del equipo',
+            'tipo': 'Tipo de equipo',
+            'marca': 'Marca del equipo',
+            'modelo': 'Modelo del equipo',
+            'precio': 'Precio de renta del equipo',
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class':'form-control'}),
+            'tipo': forms.TextInput(attrs={'class':'form-control'}),
+            'marca': forms.TextInput(attrs={'class':'form-control'}),
+            'modelo': forms.TextInput(attrs={'class':'form-control'}),
+            'precio': forms.TextInput(attrs={'class':'form-control'}),
+        }
